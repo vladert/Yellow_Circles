f24d45db23b07caa684aba7caf061b81a9151f58
vladert <vlad.simoshin@gmail.com>
2023-11-20 13:56:39 +0300

    Yellow Circles try3

--- main.py
+++ main.py
@@ -1,50 +1,50 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtGui import QPainter
 from PyQt5.QtCore import Qt
 from random import randint
-
 
 class CircleWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.diameter = 0
+        self.circles = []
 
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setBrush(Qt.yellow)
-        painter.drawEllipse(self.rect().center(), self.diameter, self.diameter)
+        for (x, y, diameter) in self.circles:
+            painter.drawEllipse(x, y, diameter, diameter)
 
-    def setDiameter(self, diameter):
-        self.diameter = diameter
+    def setCircles(self, circles):
+        self.circles = circles
         self.update()
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle('Генерация рандомного круга')
         self.setGeometry(100, 100, 400, 400)
-
         self.centralWidget = QWidget(self)
         self.setCentralWidget(self.centralWidget)
-
         self.layout = QVBoxLayout()
         self.centralWidget.setLayout(self.layout)
 
         self.circleWidget = CircleWidget()
         self.layout.addWidget(self.circleWidget)
 
-        self.button = QPushButton('Create Circle')
-        self.button.clicked.connect(self.createCircle)
+        self.button = QPushButton('Create Circles')
+        self.button.clicked.connect(self.createCircles)
         self.layout.addWidget(self.button)
 
-    def createCircle(self):
-        diameter = randint(10, 100)
-        self.circleWidget.setDiameter(diameter)
-
+    def createCircles(self):
+        circles = []
+        for _ in range(randint(1, 10)):
+            diameter = randint(10, 100)
+            x = randint(0, self.width() - diameter)
+            y = randint(0, self.height() - diameter)
+            circles.append((x, y, diameter))
+        self.circleWidget.setCircles(circles)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
